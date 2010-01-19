89a5fdd5f820ee0c2232df9722ac6388483d44a7
finfou <finfou@7dd20f14-ff25-11de-ae88-2ffea97532b2>
2010-01-19 09:22:25 +0000

    Add Preferred Language class

---
+++ wpfumprototype/WPFDemoProjects/BusinessLib/DataModel/PreferredLanguage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLib.DataModel
+{
+    public class PreferredLanguage
+    {
+        private PreferredLanguage()
+        {
+        }
+        public static PreferredLanguage Instance = new PreferredLanguage();
+        public List<string> SupportedLanguages = new List<string>{
+                                                     "English(United Kingdom)", "English(United Stated", "German(Germany)",
+                                                     "Italian(Italy)", "French(France)", "Spainish(Spain)"
+                                                 };
+    }
+}
